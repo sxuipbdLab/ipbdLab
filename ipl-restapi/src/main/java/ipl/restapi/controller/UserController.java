94783dce3fd0590635e3b0e1892751f27ab3b405
王海 <Wang6Hai@gmail.com>
2018-03-18 22:47:32 +0800

    单点登录架构-Beta

--- ipl-restapi/src/main/java/ipl/restapi/controller/UserController.java
+++ ipl-restapi/src/main/java/ipl/restapi/controller/UserController.java
@@ -4,9 +4,11 @@ import ipl.common.utils.JacksonUtil;
 import ipl.manager.pojo.Users;
 import ipl.restapi.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -23,8 +25,11 @@ public class UserController {
     @Autowired
     private UsersService usersService;
 
-    @RequestMapping("/users/{userId}")
+    // 可以匹配多个value,produces属性避免乱码
+    @RequestMapping(value = "/users/{userId}", method = RequestMethod.GET,
+            produces = {MediaType.APPLICATION_JSON_VALUE, "application/json;charset=UTF-8"})
     @ResponseBody
+    // 用于将请求URL中的模板变量映射到功能处理方法的参数上，即取出uri模板中的变量作为参数
     public String getUserJson(@PathVariable long userId) {
         Users user = usersService.getUserById(userId);
         //  取出user的所有属性，解析为前端所需要的json格式，返回给前端
