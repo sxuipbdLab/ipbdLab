7df22a8c6f0fb1ab5e9b28e53d889fd3bb399c8b
arno773877440 <zhianyuan@foxmail.com>
2018-03-31 20:29:35 +0800

    测试检索接口

--- ipl-restapi/src/main/java/ipl/restapi/controller/CollectController.java
+++ ipl-restapi/src/main/java/ipl/restapi/controller/CollectController.java
@@ -4,22 +4,11 @@ import ipl.common.utils.JacksonUtil;
 import ipl.common.utils.ResultFormat;
 import ipl.manager.pojo.Collect;
 import ipl.restapi.service.CollectService;
-import org.apache.commons.httpclient.Cookie;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.NameValuePair;
-import org.apache.commons.httpclient.cookie.CookiePolicy;
-import org.apache.commons.httpclient.methods.PostMethod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
 import java.util.Date;
 import java.util.List;
 
@@ -143,133 +132,6 @@ public class CollectController {
         }
         String data = JacksonUtil.bean2Json(collect);
         return JacksonUtil.bean2Json(ResultFormat.build("911","删除收藏信息成功返回信息成功",0,"collect",data));
-    }
-
-    @RequestMapping(value = "/getUrl", method = {GET, POST},
-            produces = {MediaType.APPLICATION_JSON_VALUE})
-    @ResponseBody
-    public Object getUrl(@RequestParam(value = "Url")String Url,@RequestParam(value = "dp") int dp){
-
-        StringBuilder sb = null;
-        // 登陆 Url
-        System.out.println(Url);
-        try {
-            Url = URLEncoder.encode(Url,"UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        System.out.println(Url);
-        //String loginUrl = "http://bkjw.sxu.edu.cn/";
-        String loginUrl = "http://172.21.201.131/search/user/login";
-        // 需登陆后访问的 Url
-        //String dataUrl = "http://bkjw.sxu.edu.cn/MAINFRM.aspx";
-
-        String dataUrl = "http://172.21.201.131/search/pub/ApiSearch?dp=" + dp + "&pn=10&fl=TI,PN,AN,PD,AU,AD,LS,AB,PA&q=TI=" +Url;
-
-        HttpClient httpClient = new HttpClient();
-
-        // 模拟登陆，按实际服务器端要求选用 Post 或 Get 请求方式
-        PostMethod postMethod = new PostMethod(loginUrl);
-
-        // 设置登陆时要求的信息，用户名和密码
-        NameValuePair[] data = { new NameValuePair("name", "webmaster"), new NameValuePair("pwd", "dfld1234") };
-        //NameValuePair[] data = {new NameValuePair("Login.Token1","201502401146"),new NameValuePair("Login.Token2","180026")};
-        postMethod.setRequestBody(data);
-
-        try {
-            // 设置 HttpClient 接收 Cookie,用与浏览器一样的策略
-            httpClient.getParams().setCookiePolicy(CookiePolicy.BROWSER_COMPATIBILITY);
-            int statusCode = httpClient.executeMethod(postMethod);
-
-            // 获得登陆后的 Cookie
-            Cookie compCookie = new Cookie();
-            Cookie[] cookies = httpClient.getState().getCookies();
-            StringBuffer tmpcookies = new StringBuffer();
-
-            for (Cookie c : cookies) {
-                tmpcookies.append(c.toString() + ";");
-                System.out.println("cookies = " + c.toString());
-            }
-
-            URL url = new URL(dataUrl);
-            URLConnection conn = url.openConnection();
-            conn.setRequestProperty("Cookie", tmpcookies.toString());
-            conn.setDoInput(true);
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            sb = new StringBuilder();
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                System.out.println("line = ****   " + line);
-                sb.append(line);
-            }
-            System.out.println(sb);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return sb;
-    }
-
-    @RequestMapping(value = "/getFullText", method = {GET, POST},
-            produces = {MediaType.APPLICATION_JSON_VALUE})
-    @ResponseBody
-    public Object getUrl(@RequestParam(value = "docPIN")String docPIN,@RequestParam(value = "docAN")String docAN,@RequestParam(value = "docPD") String docPD,@RequestParam(value = "mid") String mid){
-
-        StringBuilder sb = null;
-        // 登陆 Url
-        System.out.println(docPIN);
-        System.out.println(docAN);
-        System.out.println(docPD);
-        System.out.println(mid);
-
-        //String loginUrl = "http://bkjw.sxu.edu.cn/";
-        String loginUrl = "http://172.21.201.131/search/user/login";
-        // 需登陆后访问的 Url
-        //String dataUrl = "http://bkjw.sxu.edu.cn/MAINFRM.aspx";
-
-        String dataUrl = "http://172.21.201.131/search/pub/ApiDocinfo?un=103&sid=103&fk=FT,TI&dk=[{\"DCK\":\""+docAN+"@"+docPIN+"@"+docPD+"\",\"MID\":\""+mid+"\"}]";
-        System.out.println(dataUrl);
-
-        HttpClient httpClient = new HttpClient();
-
-        // 模拟登陆，按实际服务器端要求选用 Post 或 Get 请求方式
-        PostMethod postMethod = new PostMethod(loginUrl);
-
-        // 设置登陆时要求的信息，用户名和密码
-        NameValuePair[] data = { new NameValuePair("name", "webmaster"), new NameValuePair("pwd", "dfld1234") };
-        //NameValuePair[] data = {new NameValuePair("Login.Token1","201502401146"),new NameValuePair("Login.Token2","180026")};
-        postMethod.setRequestBody(data);
-
-        try {
-            // 设置 HttpClient 接收 Cookie,用与浏览器一样的策略
-            httpClient.getParams().setCookiePolicy(CookiePolicy.BROWSER_COMPATIBILITY);
-            int statusCode = httpClient.executeMethod(postMethod);
-
-            // 获得登陆后的 Cookie
-            Cookie compCookie = new Cookie();
-            Cookie[] cookies = httpClient.getState().getCookies();
-            StringBuffer tmpcookies = new StringBuffer();
-
-            for (Cookie c : cookies) {
-                tmpcookies.append(c.toString() + ";");
-                System.out.println("cookies = " + c.toString());
-            }
-
-            URL url = new URL(dataUrl);
-            URLConnection conn = url.openConnection();
-            conn.setRequestProperty("Cookie", tmpcookies.toString());
-            conn.setDoInput(true);
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            sb = new StringBuilder();
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                System.out.println("line = ****   " + line);
-                sb.append(line);
-            }
-            System.out.println(sb);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return sb;
     }
 
 }
